0d03290463776e2d76779d74361db3e922a16573
me7aben <bpareja.me@gmail.com>
2025-11-05 14:02:41 -0500

    Actualización de la función principal para acceder a los datos

--- Momentos/MomentosApp.swift
+++ Momentos/MomentosApp.swift
@@ -6,12 +6,23 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
 struct MomentosApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+    
+    // Crea una instancia de nuestro DataContainer.
+            // 'includeSampleMoments' es falso por defecto,
+            // así que esto creará una base de datos real en el dispositivo.
+            let dataContainer = DataContainer()
+
+            var body: some Scene {
+                WindowGroup {
+                    ContentView()
+                        // Inyecta el DataContainer en el entorno de SwiftUI
+                        .environment(dataContainer)
+                }
+                // Proporciona el ModelContainer de SwiftData a toda la app
+                .modelContainer(dataContainer.modelContainer)
+            }
 }
