6fc9b282876c306ae32c3765934710c055cdaa7f
me7aben <bpareja.me@gmail.com>
2025-11-05 11:59:42 -0500

    Initial Commit

---
+++ Momentos/MomentosApp.swift
@@ -0,0 +1,17 @@
+//
+//  MomentosApp.swift
+//  Momentos
+//
+//  Created by Brian Benjamin Pareja Meruvia on 5/11/25.
+//
+
+import SwiftUI
+
+@main
+struct MomentosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
